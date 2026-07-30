2e1d823eec9d42541d89756fb4c63141c6cb02a6
cacharrearconjuan <cacharrearconjuan@gmail.com>
2026-07-30 16:16:25 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Iniciando compilación en contenedor Docker..."
-docker build -t ps5-autoupdater-builder .
-docker run --rm -v "$(pwd):/workspace" ps5-autoupdater-builder
-echo "Compilación finalizada."
